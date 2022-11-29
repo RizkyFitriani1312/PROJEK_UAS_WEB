9943ea5d5b85d05340361ce6c4e8d76569b79898
LidyaSimanungkalit <102159302+LidyaSimanungkalit@users.noreply.github.com>
2022-11-29 23:31:21 +0800

    Delete uasweb directory

--- uasweb/PA_WEB/user/hapus_produk.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-$id = $_GET['id_produk'];
-unset($_SESSION['keranjang'] [$id]);
-
-echo "<script> alert('berhasil di hapus');</script>";
-echo "<script> location='keranjang.php'; </script>";
-
-?>
